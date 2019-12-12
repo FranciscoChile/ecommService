0e0e9c5b864f337da8297fc1ad3336b625c51a10
francisco <francisco.lopezcastro78@gmail.com>
2019-12-12 18:30:01 -0300

    mantenedor productos

--- src/main/java/ssmph/controller/ProductController.java
+++ src/main/java/ssmph/controller/ProductController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import ssmph.model.Product;
 import ssmph.service.ProductService;
@@ -19,6 +21,7 @@ import ssmph.service.ProductService;
  */
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.DELETE})
 public class ProductController {
 
     @Autowired
